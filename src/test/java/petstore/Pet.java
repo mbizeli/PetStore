221ae2262c3a46dd469e9d3f4ed340223ba92137
mbizeli <mbizeli@gmail.com>
2021-08-25 10:32:06 -0300

    Completei o scripts consultarPet (GET)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -1,4 +1,4 @@
-// 1 - pacote contendo as classes que ser√£o criados
+// 1 - pacote contendo as classes que ser„o criados
 package petstore;
 
 // 2 - Bibliotecas
@@ -13,36 +13,60 @@ import static org.hamcrest.Matchers.contains;
 // 3 - Classe
 public class Pet {
 
-    // 3.1 - atributos (como os adjetivos, caracter√≠sticas do objeto tais como, peso, cor, cpf, rg)
-    String uri = "https://petstore.swagger.io/v2/pet"; //endere√ßo da entidade pet
+    // 3.1 - atributos (como os adjetivos, caracterÌsticas do objeto tais como, peso, cor, cpf, rg)
+    String uri = "https://petstore.swagger.io/v2/pet"; //endereÁo da entidade pet
 
-    // 3.2 - m√©todos (a√ß√µes que n√£o retornam nenhum valor) e fun√ß√µes (a√ß√µes que devolvem um resultado)
+    // 3.2 - mÈtodos (aÁıes que n„o retornam nenhum valor) e funÁıes (aÁıes que devolvem um resultado)
     public String lerJson(String caminhoJson)  throws IOException {
        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
     //Incluir - Create - Post
-    @Test // identifica o m√©todo ou fun√ß√£o como um teste para o TestNG
+    @Test(priority = 1) // identifica o mÈtodo ou funÁ„o como um teste para o TestNG
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
 
-        //sint√°xe Gherkin
-        //Dado - Quando - Ent√£o
+        //sint·xe Gherkin
+        //Dado - Quando - Ent„o
         //Given - When - Then
 
         given()  // Dado
                 .contentType("application/json") // comum em API REST - as antigas eram "text/xml"
                 .log().all()
-                .body(jsonBody) //corpo da requisi√ß√£o
+                .body(jsonBody) //corpo da requisiÁ„o
         .when() // Quando
                 .post(uri)
-        .then() // Ent√£o
+        .then() // Ent„o
                 .log().all()
                 .statusCode(200)
                 .body("name", is("Thor")) //corpo da resposta
                 .body("status",is("available"))
-                .body("category.name", is ("dog"))
-                .body("tags.name", contains("vacinado"))
+                .body("category.name", is ("ASD4535HGJ"))
+                .body("tags.name", contains("data"))
+
         ;
+    }
+
+    //consulta um pet - metodo GET
+    @Test(priority = 2)
+    public void consultarPet(){
+        String petID = "1971110322";
+        String token =
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + "/" + petID)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Thor"))
+                .body("category.name", is("ASD4535HGJ"))
+                .body("status", is("available"))
+        .extract()
+                .path("category.name")
+
+        ;
+        System.out.println("O token È " + token);
     }
 }
